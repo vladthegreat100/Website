28f3061ccde4bee96a5415e1fda98a89a30f4c01
Yuri <vYuri@users.noreply.github.com>
2016-01-10 04:16:42 -0300

    Inde'

---
+++ html/tournaments/index.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<title>Tournament - Override</title>
+		<link rel="shortcut icon" href="images/favicon.png">
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+		<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="..\css\main.css">
+		<link rel="stylesheet" href="..\css\animate.css">
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<div class="container">
+			<div class="page-header">
+				<h2>
+					Tournaments
+					<small></small>
+					</a>
+				</h2>
+			</div>
+		</div>
