ad315f47e241c728fe98e3654a78d8d6a74732fb
Yuri <vYuri@users.noreply.github.com>
2016-02-05 02:52:18 -0300

    Hacks 1

--- html/rules/index.php
+++ html/rules/index.php
@@ -1,10 +1,36 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Override - Rules</title>
-</head>
+	<head>
+		<title>Rules - Override</title>
+		<link rel="shortcut icon" href="images/favicon.png">
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+		<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="..\css\main.css">
+		<link rel="stylesheet" href="..\css\animate.css">
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	</head>
 <body>
-	
+	<b>Hacked Clients</b>
+<b>The mods listed below are permitted for use throughout Override.</b>
+		1. Optifine
+		2. Gammabright
+		3. Mods that change Minecraft's graphical user interface
+		4. Toggle Sprint
+		5. Rei.s minimap
+		6. 5zig PvP Modification
+		
+<b>The mods listed below are strictly prohibited on the Override Network.</b>
+		1. Sorting inventory mods
+		2. Fly mods
+		3. Aimbots
+		4. Toggle-crouch mods that allow crouching while using a Minecraft interface (inventories, menus, chat)
+		5. Minimaps that reveals the positions of creatures, players or underground caves/tunnels
+		6. Auto-swing mods
+		7. Mods that assist in switching tools
+		8. X-ray mods or texture packs
+		9. Mods that enhance player visibility (enlarged name tags)
+		10. Mods that enhance sprinting capabilities
 		<hr width="105%" />
 		<footer class="footer">
 			<div class="container">
