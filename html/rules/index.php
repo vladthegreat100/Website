c8f7a7db60f6fdbdf37d8353381aeff693c05989
Yuri <vYuri@users.noreply.github.com>
2016-02-05 03:08:01 -0300

    Update index.php

--- html/rules/index.php
+++ html/rules/index.php
@@ -11,32 +11,313 @@
 		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	</head>
 <body>
-	<b>Hacked Clients</b>
-<div class="row">
-	<div class="span12">
-<ol>
-<b>The mods listed below are permitted for use throughout Override.</b>
-		1. Optifine
-		2. Gammabright
-		3. Mods that change Minecraft's graphical user interface
-		4. Toggle Sprint
-		5. Rei.s minimap
-		6. 5zig PvP Modification
-		
-<b>The mods listed below are strictly prohibited on the Override Network.</b>
-		1. Sorting inventory mods
-		2. Fly mods
-		3. Aimbots
-		4. Toggle-crouch mods that allow crouching while using a Minecraft interface (inventories, menus, chat)
-		5. Minimaps that reveals the positions of creatures, players or underground caves/tunnels
-		6. Auto-swing mods
-		7. Mods that assist in switching tools
-		8. X-ray mods or texture packs
-		9. Mods that enhance player visibility (enlarged name tags)
-		10. Mods that enhance sprinting capabilities
-</ol>
-	</div>
-		</div>
+    <h1>Rules</h1>
+    <div>
+      <hr />
+
+<h4>These rules serve as a guideline for punishments for illegal actions, keep in mind that any staff member has the ability to soften or harden these punishments if they find it necessary. If you feel that you have been banned in-justly or would like to report somebody for breaking one of these rules, please <a href="http://kohi.net/support">send us a ticket</a>.</h4>
+
+<h4>Rules may be added or changed at any time. It is up to the players to keep up to date on these rules.</h4>
+
+<hr />
+
+<h4>Information on Blacklists</h4>
+
+<ul>
+<li><p>Blacklists are entirely up to the admins discretion and can not be appealed.</p></li>
+<li><p>If you are blacklisted it means we do not want you on the Network, at all.</p></li>
+<li><p>Players who purchase  the limit of 2 unbans will be blacklisted if they are banned again.</p></li>
+</ul>
+
+<hr />
+
+<h4>Hacked Clients</h4>
+
+<p>Any mod that changes the way a player interacts with the server is considered a hacked client. This contains things such as force field, click aimbot, flying, etc.</p>
+
+<ul>
+<li>First offense - Permanent Ban  </li>
+</ul>
+
+<hr />
+
+<h4>Lying to Staff Members</h4>
+
+<ul>
+<li>First offense - Permanent Ban
+Simple enough, do not lie to any staff member.</li>
+</ul>
+
+<hr />
+
+<h4>X-Ray/Radar</h4>
+
+<ul>
+<li>First Offense - 1 Month Ban</li>
+<li>Second Offense - Permanent Ban</li>
+</ul>
+
+<hr />
+
+<h4>Any Other Unapproved Texture Packs and Mods</h4>
+
+<p>Anything outside of the vanilla client used to provide an advantage to player that isn't considered a hack falls under this category. Mods that you have not been given permission to use are NOT allowed.</p>
+
+<ul>
+<li>First Offense - 2 Week Ban</li>
+<li>Second Offense - Permanent Ban</li>
+</ul>
+
+<hr />
+
+<h4>Auto Clickers</h4>
+
+<p>Modifying your mouse in ANYWAY Is not allowed
+Doing anything to make your mouse double click or click twice in one motion is not allowed
+Keep in mind doing things like butterfly clicking can make your mouse double click (click twice in one movement) and can get you banned</p>
+
+<ul>
+<li>First Offense - Permanent Ban</li>
+</ul>
+
+<hr />
+
+<h4>Macroing</h4>
+
+<p>Auto clickers and macros for your mouse are NOT allowed neither is rebinding mouse buttons or using more than one device at a time. And Yes, it is still illegal even if you were using your mouse while your dog/hamster/grandmother etc was using a second device. Dragclicking is also not allowed, neither is any other stupid crap you guys come up with in future.</p>
+
+<ul>
+<li>First Offense - Permanent Ban</li>
+</ul>
+
+<hr />
+
+<h4>Abuse or Exploit of a Bug or Glitch</h4>
+
+<p>Exploiting any bug, whether Minecraft related (F3 + A) or plugin related, for the use of personal gain or causing harm to the server or its players is bannable. Maliciously using the factions plugin in ways that it is not meant for is considered abusing, this means things like storage factions and kicking to avoid power loss are not allowed. If you aren't sure if something is allowed, ask before you do it to prevent a possible ban. If you find a bug, please report it immediately to the admins through either private message or the <a href="http://kohi.net/support">ticket system</a>.</p>
+
+<ul>
+<li>First Offense - 2 Week Ban</li>
+<li>Second Offense - Permanent Ban</li>
+</ul>
+
+<hr />
+
+<h4>Block Glitching</h4>
+
+<p>Block Glitching involves hitting a player through a block.</p>
+
+<ul>
+<li>First Offense - Warning</li>
+<li>Second Offense - Scaling bans</li>
+</ul>
+
+<hr />
+
+<h4>DTR evasion</h4>
+
+<p>Kicking members during/before PVP in order to prevent DTR loss and going raidable.</p>
+
+<ul>
+<li>First offense - 3 day ban</li>
+<li>Second Offense - 7 day ban</li>
+<li>Third offense - 28 day ban</li>
+<li>Fourth offense - Perm ban</li>
+</ul>
+
+<p>All players involved will be banned, this includes faction admins/mods who were online at the time. You are not allowed to leave your faction in order to pvp and avoid dtr loss.</p>
+
+<hr />
+
+<h4>Inappropriate Chat Content</h4>
+
+<ul>
+<li>Players who say inappropriate or disrespect things will be muted ( temp or perm) or banned (temp or perm) based on context and previous offenses.</li>
+<li><p>Players may also be Blacklisted for this if they are considered serial offenders.</p>
+
+<p>These can be appealed</p></li>
+<li><p>Abuse of /report and /helpop is punishable and will result in temp bans ranging in time depending on context/volume. These punishments are up to the admins discretion.</p></li>
+<li><p>Organizing a /report of /helpop  spam will result in a temporary ban, starting at 1 hour.</p></li>
+</ul>
+
+<p>Players who we consider serial offenders (people who we find to be considerably toxic in chat) will be permanently muted</p>
+
+<hr />
+
+<h4>Staff Disrespect</h4>
+
+<p>Saying anything to a staff member which involves their personal life in anyway will get you muted/banned depending on context/how inappropriate the content is. The punishment will be decided by an admin and serial offenders may be permanently banned.</p>
+
+<ul>
+<li>Any Offense - Punishment decided by an admin</li>
+</ul>
+
+<p>This has become a big problem recently and it is not acceptable.</p>
+
+<hr />
+
+<h4>Death Threats/ Suicide jokes/Telling someone to kill themselves/Scamming</h4>
+
+<ul>
+<li>Punishment decided by Admin</li>
+</ul>
+
+<p>Racism will not be tolerated, harassment of our players and staff will not be tolerated, and spamming will not be tolerated. Scamming i.e. promising any purchasable items (CS: GO items, server ranks etc.) in exchange for in game items (diamonds and whatever else it is you guys are too lazy to actually play the game and get) or to join a faction, with the intent of not following through is NOT allowed in chat. If you are seen doing this in chat you will be muted, how long you are muted is up to the admins. Use common sense when typing in chat. If it isn't something you'd say to someone's face, then it isn't something you should say here. Releasing personal information about players includes but is not limited to facebook/instagram accounts, phone number, skype names, addresses, or pictures. This will result in a temp/permban or a blacklist depending on the situation. To what extent the offender went to obtain the information is also a factor, if you they got your picture from a faction irl video don't expect us ban them for "doxing".</p>
+
+<hr />
+
+<h4>Advertising of another Server</h4>
+
+<p>Simple enough, don't advertise. This does not include talking about another server in conversation.</p>
+
+<ul>
+<li>First Offense - Permanent Ban</li>
+</ul>
+
+<hr />
+
+<h4>Playing On or Using Alternate Accounts</h4>
+
+<p>The use of alternate accounts is not allowed on ANY of our servers. If you have any family/friends that share an IP with you, you are required to tell us through the <a href="http://kohi.net/support">ticket system</a> so that we do not accidentally ban you.</p>
+
+<ul>
+<li>First Offense - 1 Month Ban</li>
+<li>Second Offense - Permanent Ban</li>
+</ul>
+
+<p>Alting on the Practice servers is not allowed</p>
+
+<ul>
+<li>First Offense - The alt will be banned, you will receive a warning.</li>
+<li>Second Offense - 2 week ban</li>
+</ul>
+
+<hr />
+
+<h4>VPNS/Shared Accounts</h4>
+
+<ul>
+<li><p>The use of VPN's is a bannable offense. </p></li>
+<li><p>Sharing accounts is a bannable offense. </p></li>
+</ul>
+
+<hr />
+
+<h4>Illegal Traps</h4>
+
+<p>Traps need to either kill players or give them the ability to /f home. A trap that does fails to do either of those is considered illegal. Walled up nether portals on claimed land are considered to be illegal traps, aswell as nether portal fall traps.</p>
+
+<ul>
+<li>First Offense - 1 Week Ban</li>
+<li>Second Offense - 1 Month Ban</li>
+<li>Third Offense - Permanent Ban</li>
+</ul>
+
+<hr />
+
+<h4>DDoSing/DoSing/Doxing</h4>
+
+<p>These are considered federal crimes in most countries and will NOT be tolerated on our servers. If you have any evidence of somebody committing these acts, please show us through the <a href="http://kohi.net/support">ticket system</a>. This includes ALL DDoS threats, joke or not.</p>
+
+<ul>
+<li>First Offense - Blacklist (permanent ban with no ability to buy an unban)</li>
+</ul>
+
+<hr />
+
+<h4>Cobble Monsters/Water Griefing/Bypass Faction protection</h4>
+
+<p>Includes anything that bypasses claim protection to create/destroy blocks in another factions claim when you shouldn't be able to</p>
+
+<ul>
+<li>Scaling bans (starting at 3 days)</li>
+</ul>
+
+<hr />
+
+<h4>Inappropriate Names</h4>
+
+<p>Inappropriate names will be perm banned until the name is changed. This any name including the words "Nigger", "Faggot", "DDOS" or anything overly sexual. If you would like to get an approved alt in the meantime please let a member of staff know here - http://kohi.net/support</p>
+
+<hr />
+
+<h4>Camping in Practice Servers</h4>
+
+<p>Camping and running to avoid the fight is not allowed.</p>
+
+<ul>
+<li><p>First Offense - Warning</p></li>
+<li><p>Second Offense - Kick</p></li>
+<li><p>Third Offense - Scaling bans</p></li>
+</ul>
+
+<hr />
+
+<h4>Betraying</h4>
+
+<p>Betraying or insiding a faction will result in the betrayer rank being assigned to you. With this rank, you will no longer have your donation prefix, but you will still receive the benefits. This rank will give you a 12 hour deathban if you do not have a rank. Ranks will still shorten deathbans by the correct percentage with the insider rank. Basic=10.8 hours Silver=9 hours Gold=6.6 hours Platinum=4.2 hours.</p>
+
+<p>If the betrayed items can be retrieved, staff will give them back to their original owners.</p>
+
+<hr />
+
+<h4>Trading</h4>
+
+<p>Do not advertise that you are looking to buy, sell or trade anything.</p>
+
+<p>This includes but is not limited to:</p>
+
+<ul>
+<li>CSGO skins or items of value on other games</li>
+<li>Minecraft accounts and capes</li>
+<li>Money</li>
+<li>Ranks on Kohi or any other minecraft server</li>
+</ul>
+
+<p>The only exception is ingame goods or services for other ingame goods or services.</p>
+
+<hr />
+
+<h4>Hackusating in chat</h4>
+
+<p>If you believe someone is cheating, you would /report him, and not put it in public chat. </p>
+
+<ul>
+<li><p>First Offense - 1 hour mute</p></li>
+<li><p>Second Offense - 6 hour mute</p></li>
+<li><p>Third Offense - 24 hour mute</p></li>
+<li><p>Continued offences will result in a temp ban.</p></li>
+</ul>
+
+<h4>Rank transfers / Switching accounts are NOT allowed anymore.</h4>
+
+<hr />
+
+<h4>Team Griefing</h4>
+
+<p>If you are intentionally griefing your own team in any of our gamemodes, you will be banned.</p>
+
+<ul>
+<li><p>First Offense - 1 hour ban</p></li>
+<li><p>Second Offence - 3 hours</p></li>
+<li><p>Third Offense -  Up to the admins discretion</p></li>
+</ul>
+
+<hr />
+
+<h4>Allying on factions</h4>
+
+<p>You may not ally with another faction in combat. However, if your faction has two or less members online you may ally to fight with one other faction who also has two or less members online. </p>
+
+<ul>
+<li><p>First Offense - 1 day ban for all members involved.</p></li>
+<li><p>Second Offense - 5 day ban for all members involved.</p></li>
+<li><p>Third Offense - Up to admins discretion.</p></li>
+</ul>
+
+    </div>
+  
+</div>
 		<hr width="105%" />
 		<footer class="footer">
 			<div class="container">
