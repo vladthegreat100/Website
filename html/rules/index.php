01cad534eaeb11732344a659d26e182b2834dc19
Yuri <vYuri@users.noreply.github.com>
2016-02-05 02:47:12 -0300

    Rules1

--- html/rules/index.php
+++ html/rules/index.php
@@ -5,5 +5,21 @@
 </head>
 <body>
 	
-</body>
+		<hr width="105%" />
+		<footer class="footer">
+			<div class="container">
+				<div class="pull-left">
+					&copy; 2016 Override Network
+				</div>
+				<div class="pull-right">
+					<a href="https://twitter.com/OverrideNet"><i class="fa fa-twitter fa-lg"></i></a>
+					|
+					<a href="http://overridenetwork.net/forums">Forums</a>
+					|
+					<a href="http://overridenetwork.net">Home</a>
+				</div>
+			</div>
+			<br>
+		</footer>
+	</body>
 </html>
