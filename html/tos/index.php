f2ffa3ef5a16a59a0fe0dcd4dfa0c768615c660c
alexanderjoe <alexanderjoemc@gmail.com>
2016-04-04 20:26:27 -0400

    Add TOS

---
+++ html/tos/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>OverrideNetwork - Terms of Service</title>
+	<meta name="description" content="OverrideNetwork Home">
+	<meta name="keywords" content=" ">
+	<meta name="Author" content="CodedGuy and alexanderjoe">
+		
+	<link rel="stylesheet" href="../css/index.css" type="text/css" media="screen">
+		
+	<link rel="stylesheet" href="../css/font-awesome.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="../css/font-awesome.min.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>	
+</head>
+<body>
+	<div class="container">
+		<!-- NAV -->
+		<div class="navbar">
+			<a href="#" id="logo">
+				OverrideNetwork
+			</a>
+			<div id="right">
+				<a href="http://overridenetwork.net/">
+					<i class="glyphicon glyphicon-home"></i>
+					Home
+				</a>
+				<a href="http://overridenetwork.net/forums/">
+					<i class="glyphicon glyphicon-book"></i>
+					Forums
+				</a>
+				<a href="http://shop.overridenetwork.net/">
+					<i class="glyphicon glyphicon-shopping-cart"></i>
+					Shop
+				</a>
+				<a href="http://overridenetwork.net/staff/">
+					<i class="glyphicon glyphicon-user"></i>
+					Staff
+				</a>
+			</div>
+		</div>
+		<!-- END NAV -->
+		<!-- TOS -->
+			<h2 class="align-center well">Hello user! This page is currently under construction. <small>Check back later.</small></h2>
+		<!-- END TOS -->	
+
+		<!-- FOOTER -->
+			<div class="footer">
+			<span style="color:#27303a;font-size: 12pt;"> Override Network &copy; 2016</span>
+			</div>
+		<!-- END FOOTER -->
+	</div>
+</body>
+</html>
