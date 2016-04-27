46432e77c2bb7427572453f5e4dc1a43a89a3987
alexanderjoe <alexanderjoemc@gmail.com>
2016-04-26 20:25:55 -0400

    Test area

---
+++ html/test/forums/connect.php
@@ -0,0 +1,3 @@
+<?php 
+$db = new mysqli("localhost","root","","forum") or die("FATAL ERROR - COULD NOT CONNECT!");
+?>
---
+++ html/test/forums/post.php
@@ -0,0 +1,2 @@
+<div class="post"><a href="forum.php?id=<?php echo $p_id?>" style="color: ;"> <?php echo $p_title?></a><span style="color: red;"> 
+<br><?php echo "posted by:";?></span> <?php echo $p_auth?></div><hr>
