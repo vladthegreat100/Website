ed90e843b42fb22c4d0210cdba966e1b3e6d4f5c
alexanderjoe <alexanderjoemc@gmail.com>
2016-04-26 20:33:39 -0400

    fix connect

--- html/test/forums/connect.php
+++ html/test/forums/connect.php
@@ -1,3 +1,3 @@
 <?php 
-$db = new mysqli("localhost","root","","forum") or die("FATAL ERROR - COULD NOT CONNECT!");
+$db = new mysqli("localhost","root","gOefoU3Lu4oapOa","forumtest") or die("FATAL ERROR - COULD NOT CONNECT!");
 ?>
