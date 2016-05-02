06ce8cf03fbf0dd4c626315e8c90301f6ea595ed
alexanderjoe <alexanderjoemc@gmail.com>
2016-05-01 23:54:54 -0400

    Forgotten like the rest

--- html/test/forums/connect.php
+++
@@ -1,3 +0,0 @@
-<?php 
-$db = new mysqli("localhost","root","gOefoU3Lu4oapOa","forumtest") or die("FATAL ERROR - COULD NOT CONNECT!");
-?>
--- html/test/forums/post.php
+++
@@ -1,2 +0,0 @@
-<div class="post"><a href="forum.php?id=<?php echo $p_id?>" style="color: ;"> <?php echo $p_title?></a><span style="color: red;"> 
-<br><?php echo "posted by:";?></span> <?php echo $p_auth?></div><hr>
