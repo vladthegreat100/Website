ef7874640130bb65055ba363302f52a5dabc8578
alexanderjoe <alexanderjoemc@gmail.com>
2016-05-08 01:05:29 -0400

    Delete test

--- html/shell/testrun.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_GET['run']) {
-  # This code will run if ?run=true is set.
-  exec("/root/Servers/restartServers.sh");
-}
-?>
-
-<!-- This link will add ?run=true to your URL, myfilename.php?run=true -->
-<a href="?run=true">Click Me!</a>
