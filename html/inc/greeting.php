1f9219e3499313bdb2030d671a1c103ed6a86714
Yuri <vYuri@users.noreply.github.com>
2016-05-25 03:14:44 -0300

    width

--- html/inc/greeting.php
+++ html/inc/greeting.php
@@ -1,4 +1,4 @@
 <div class="greeting well-g">
-		<img src='http://overridenetwork.net/forums/styles/logo.png' style='float: right;' width="120" height="120">/>
+		<img src='http://overridenetwork.net/forums/styles/logo.png' style='float: right;' width="121" height="119">
 	<p><h2>Welcome to Override Network!</h2>We offer a few different here gamemodes such as Desta, HCF, and Practice. Since the network is still in beta gamemodes can and will have bugs so dont forget to report them!</p>
 </div>
