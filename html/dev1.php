517966ba5c8645832f5440937adef8e1c2ac3a30
Yuri <vYuri@users.noreply.github.com>
2016-05-25 05:41:15 -0300

    Update and rename dev1 to dev1.php

--- html/dev1.php
+++ html/dev1.php
@@ -16,29 +16,9 @@
 </head>
 <body>
 	<div class="container">
-<div class="navbar">
-	<a href="#" id="logo">
-		Override Network
-	</a>
-	<div id="right">
-		<a href="http://overridenetwork.net/">
-			<i class="glyphicon glyphicon-home"></i>
-			Home
-		</a>
-		<a href="http://overridenetwork.net/forums/">
-			<i class="glyphicon glyphicon-book"></i>
-			Forums
-		</a>
-		<a href="http://shop.overridenetwork.net/">
-			<i class="glyphicon glyphicon-shopping-cart"></i>
-			Shop
-		</a>
-		<a href="http://overridenetwork.net/staff/">
-			<i class="glyphicon glyphicon-user"></i>
-			Staff
-		</a>
-	</div>
-</div>
+		<!-- NAV -->		
+ 			<?php include"./inc/nav.php"; ?>		
+ 		<!-- END NAV -->
 			<br>
 	<hr>
 		<!-- Server Info-->
