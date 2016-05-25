0d064491814b9cd0d22bfb89117a90191578d253
Yuri <vYuri@users.noreply.github.com>
2016-05-25 03:08:39 -0300

    Update greeting.php

--- html/inc/greeting.php
+++ html/inc/greeting.php
@@ -1,3 +1,7 @@
 <div class="greeting well-g">
 	<p><h2>Welcome to Override Network!</h2>We offer a few different here gamemodes such as Desta, HCF, and Practice. Since the network is still in beta gamemodes can and will have bugs so dont forget to report them!</p>
+<div id='container'>
+ <class="home-image" style='float: left;'/>
+ <p>some text (that will now wrap around the image</p>
+</div>
 </div>
