dadfc07e9c2de87e30ca4be6402d19ce6b6713c3
Yuri <vYuri@users.noreply.github.com>
2016-05-25 03:10:36 -0300

    Update greeting.php

--- html/inc/greeting.php
+++ html/inc/greeting.php
@@ -1,8 +1,8 @@
 <div class="greeting well-g">
 	<p><h2>Welcome to Override Network!</h2>We offer a few different here gamemodes such as Desta, HCF, and Practice. Since the network is still in beta gamemodes can and will have bugs so dont forget to report them!</p>
+</div>
 
 <div id='container'>
- 	<img src='http://www.cocinista.es/download/bancorecursos/recetas/hacer-queso-duro.jpg' style='float: left;'/>
+ 	<img src='http://overridenetwork.net/forums/styles/logo.png' style='float: left;'/>
  		<p>some text (that will now wrap around the image</p>
 	</div>
-</div>
