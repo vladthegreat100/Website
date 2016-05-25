afac2aae08d729d6d1c5864473bc62319022de16
Yuri <vYuri@users.noreply.github.com>
2016-05-25 05:00:56 -0300

    Testing

--- html/tournaments/index.php
+++ html/tournaments/index.php
@@ -30,19 +30,30 @@
 </div>
 		
 		<hr width="100%" />
-		<footer class="footer">
-			<div class="container">
-				<div class="pull-left">
-					&copy; 2016 Override Network
-				</div>
-				<div class="pull-right">
-					<a href="https://twitter.com/OverrideNet"><i class="fa fa-twitter fa-lg"></i></a>
-					|
-					<a href="http://overridenetwork.net/forums">Forums</a>
-					|
-					<a href="http://overridenetwork.net">Home</a>
-				</div>
-			</div>
-		</footer>
+        <footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Footer Content</h5>
+                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2016 Override Network
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </footer>
 	</body>
 </html>
