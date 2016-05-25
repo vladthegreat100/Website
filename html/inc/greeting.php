10b5e52a2070a37ef2b898c7316289115777df85
Yuri <vYuri@users.noreply.github.com>
2016-05-25 02:56:51 -0300

    Update greeting.php

--- html/inc/greeting.php
+++ html/inc/greeting.php
@@ -1,3 +1,3 @@
 <div class="greeting well-g">
-	<p><h2>Welcome to Override Network!</h2><br>We offer a few different here gamemodes such as Desta, HCF, and Practice. Since the network is still in beta gamemodes can and will have bugs so dont forget to report them!</p>
+	<p><h2>Welcome to Override Network!</h2>We offer a few different here gamemodes such as Desta, HCF, and Practice. Since the network is still in beta gamemodes can and will have bugs so dont forget to report them!</p>
 </div>
